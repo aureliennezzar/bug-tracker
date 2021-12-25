a770a21fdb3bee3d5e29c45208308790eb1f2e80
aureliennezzar <aurelien.nezzar@outlook.fr>
2021-12-25 18:01:11 +0100

    fetch hook added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import './App.scss'
+import useFetch from "../../hooks/useFetch";
+
+const URL = "https://jsonplaceholder.typicode.com/todos";
 
 const App = () => {
+    const {loading, error, data} = useFetch(URL);
+
+    if (loading) return 'loading...'
+    if (error) return 'error...'
 
     return (
         <>
             <h1>APP</h1>
+            {data?.map(article => (
+                <p>{article.title}</p>
+            ))}
         </>
     )
 
